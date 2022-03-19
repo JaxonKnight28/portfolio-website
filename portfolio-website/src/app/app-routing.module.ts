0b68f5a55c4077a936b4f1368fcd263e8d6a3f1d
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-03-19 10:22:50 -0600

    started basic layout and deployed on firebase

---
+++ portfolio-website/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
